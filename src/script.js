4b1f078154a9c457adfb22ab427b0ed866bb80eb
Cristiano Silva <alchaarcristiano@gmail.com>
2023-02-01 18:45:40 +0000

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,34 @@
+const buttonImage = document.getElementById("toggle-menu-img");
+const linkMenu = document.getElementById("menu");
+const mainContent = document.querySelector("main");
+
+const menuBurguerCloseSVG = '../assets/menu-buguer-close.svg'
+const menuBurguerOpenSVG = '../assets/menu-buguer-open.svg'
+
+let isToggled = false; //false if menu is not opened, true otherwise
+
+function toggleMenu(){
+    if(isToggled == false){
+        buttonImage.src = menuBurguerCloseSVG;
+        linkMenu.style.display="flex";
+        mainContent.style.display="none";
+        isToggled = true
+    }else{
+        buttonImage.src = menuBurguerOpenSVG;
+        linkMenu.style.display="none";
+        mainContent.style.display="flex";
+        isToggled = false
+    }
+}
+
+window.addEventListener('resize' , () => {
+    if(window.innerWidth <= 550 ){
+        buttonImage.src = menuBurguerOpenSVG;
+        linkMenu.style.display="none";
+        mainContent.style.display="flex";
+        isToggled = false
+    }
+    else{
+        linkMenu.style.display="flex";
+    }
+});
